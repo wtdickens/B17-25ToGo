0ecc2e463302dc3ab4a32aa7e668415efdbd35d2
Bill Dickens <wtdickens@gmail.com>
2020-08-25 23:16:02 -0400

    Work on BomberHit.py

--- BomberHit.py
+++ BomberHit.py
@@ -1,66 +1,96 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 22 15:37:43 2020
+DetermineDamage(dataDict, returnText, EventDict):
+    """
+    Created on Sat Aug 22 15:37:43 2020
+    When bomber is hit this determines the nature of the damage.
+    
+    Parameters
+    ----------
+    dataDict: dictionary
+        Contains game data
+        
+    returnText: String
+        Text account of events on current mission
+    
+    EventDict: dictionary
+        Impacts from mission event
+        
+    Returns
+    -------
+    shotDown: boolean
+        Was bomber shot down?
+        
+    returnText: String
+        Text account of events on current mission
+        
+    dataDict: dictionary
+        Contains game data
 
-@author: wtdic
-"""
+    @author: wtdic
+    """
 
- whoHit = d10()
-            returnText += "We're hit! We're hit!\n"
-            hitLocation = d10()
+    shotDown = False
+    # Determine where bomber was hit 
+    returnText += "We're hit! We're hit!\n"
+    hitLocation = d10()
             
-            # Hit other than crew member
-            if hitLocaation <= 3 or hitLocation > 7:
-                returnText += "Damage report!\n"
+    # Hit other than crew member
+    if hitLocaation <= 3 or hitLocation > 7:
+        returnText += "Damage report!\n"
                 
-                # Superficial damage
-                if hitLocaation <=3:
-                    returnText +=("Nothing serious. Just some holes in "
-                                  + bomberData["HitLocations][hitLocation])
+        # Superficial damage
+        if hitLocaation <=3:
+            returnText +=("Nothing serious. Just some holes in "
+                          + bomberData["HitLocations][hitLocation])
                 
-                # Frame Damage
-                elif hitLocation == 8:
-                    # If already damaged shot down
-                    if bomberData["DamageStatus"]["Frame"] == "damaged":
-                        returnText += "We're going down! Hit the silk!\n"\
+        # Frame Damage
+        elif hitLocation == 8:
+            # If already damaged shot down
+            if bomberData["DamageStatus"]["Frame"] != "OK":
+                returnText += "We're going down! Hit the silk!\n"\
                         "...\n...\n...\n...\n...\n"                      \
                         "You land safely but are picked up by a German\n"\
                         "patrol and spend the rest of the war in a\n"    \
                         "prison camp"
-                        return("Shot Down\n" + returnText)
-                    # If not damaged Frame damaged
-                    else:
-                        bomberData["DamageStatus"]["Frame"] = "damaged"
-                        returnText += "Wingsapr was hit bad, but it"  \
-                            " should hold for now."
+                shotDown = True
+                return(shotDown, returnText, dataDict)
+            # If not damaged Frame damaged
+            else:
+                bomberData["DamageStatus"]["Frame"] = "damaged"
+                returnText += "Wingsapr was hit bad, but it\n"  \
+                                  "should hold for now."
                             
-                # Engine Hit
-                elif hitLocation == 9:
-                    if bomberData["DamageStatus"] == "enginedamage":
-                        returnText += "Engines 3 and 4 gone\n "       \
+        # Engine Hit
+        elif hitLocation == 9:
+            engineDamage = bomberData["DamageStatus"]["Engine"]
+            # If already damaged shot down
+            if engineDamage != "OK":
+                returnText += "Engines 3 and 4 gone\n "       \
                             "We're going down. Hit the silk!\n"       \ 
                             "...\n...\n...\n...\n...\n"               \
                             "You land safely and are picked up by\n"  \
                             "the underground who smuggle you into\n"  \
                             "Switerland where you spend the rest of\n"\
                             "the war."
-                            return("Shot Down/n" + returnText)
-                    else:
-                        bomberData["DamageStatus"]
-                        returnText += "Engine 2's on fire! Blow the"   \
+                shotDown = True
+                return(shotDown, returnText, dataDict)
+            else:
+                bomberData["DamageStatus"]["Engine"] = "damaged"
+                returnText += "Engine 2's on fire! Blow the"   \
                             " extinguisher.\nFires out. Feather 2\n"   \
                             " we're good. Continue on course."
                             
-                # Shot Down
-                else:
-                    returnText += "Flames erupt all arount you. A wing" \
+        # Shot Down
+        else:
+            returnText += "Flames erupt all arount you. A wing" \
                         " has separated from the plane. The bomber "    \
                         " begins to spin maddly. You pass out.\n"       \
                         " Your war is over..."
-                        return("Shot Down/n" + returnText)
+            shotDown = True
+            return(shotDown, returnText, dataDict)
                     
             # Already wounded/killed crew member is hit
-            elif bomberData["CrewStatus"][whoHit] != "healthy":
+    elif bomberData["CrewStatus"][whoHit] != "healthy":
                 returnText += "Whoa that was close. Bullet just missed"
                 
             # Crew member wounded
